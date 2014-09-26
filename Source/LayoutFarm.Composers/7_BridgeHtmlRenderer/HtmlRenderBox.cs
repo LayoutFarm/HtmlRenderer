0b69d112922f70ab9e70a640d07ea60d29ba0d81
win <wintercoredev@gmail.com>
2014-09-26 18:40:02 +0700

    7459.1

---
+++ Source/LayoutFarm.Composers/7_BridgeHtmlRenderer/HtmlRenderBox.cs
@@ -0,0 +1,71 @@
+﻿//2014 Apache2, WinterDev
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using HtmlRenderer.WebDom;
+using LayoutFarm.Drawing;
+using HtmlRenderer;
+
+using HtmlRenderer.Css;
+using HtmlRenderer.ContentManagers;
+using HtmlRenderer.Composers;
+
+namespace LayoutFarm
+{
+
+    public class HtmlRenderBox : RenderBoxBase
+    {
+        MyHtmlIsland _htmlIsland;
+
+        int myWidth;
+        int myHeight;
+        public HtmlRenderBox(RootGraphic rootgfx,
+            int width, int height,
+            MyHtmlIsland htmlIsland)
+            : base(rootgfx, width, height)
+        {
+            this.myWidth = width;
+            this.myHeight = height;
+            this._htmlIsland = htmlIsland;
+
+
+        }
+        /// <summary>
+        /// Perform html container layout by the current panel client size.
+        /// </summary>
+        void PerformHtmlLayout(IGraphics g)
+        {
+            if (_htmlIsland != null)
+            {
+                _htmlIsland.MaxSize = new LayoutFarm.Drawing.SizeF(this.myWidth, 0);
+                _htmlIsland.PerformLayout(g);
+            }
+        }
+        public override void ClearAllChildren()
+        {
+
+        }
+        protected override void BoxDrawContent(Canvas canvasPage, InternalRect updateArea)
+        {
+            _htmlIsland.PhysicalViewportBound = new RectangleF(0, 0, myWidth, myHeight);
+            _htmlIsland.PerformPaint(canvasPage);
+        }
+        public override void ChildrenHitTestCore(HitPointChain artHitResult)
+        {
+            //hit test in another system 
+        }
+        public void LoadHtmlText(string html)
+        {
+            _htmlIsland.SetHtml(html, _htmlIsland.BaseStylesheet);
+            this.PerformHtmlLayout(CurrentGraphicPlatform.P.SampleIGraphics);
+
+        }
+    }
+}
+
+
+
+
+
