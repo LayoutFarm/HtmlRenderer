30c6a593dfa54c6b43110fb1d50eebb7597d1a6e
win <wintercoredev@gmail.com>
2014-10-13 17:50:38 +0700

    7659

--- Source/LayoutFarm.HtmlRenderer/2_Boxes/1_CoreBox/CssBox_Paint.cs
+++ Source/LayoutFarm.HtmlRenderer/2_Boxes/1_CoreBox/CssBox_Paint.cs
@@ -278,8 +278,14 @@ namespace HtmlRenderer.Boxes
                     GraphicsPath roundrect = null;
                     bool hasSomeRoundCorner = this.HasSomeRoundCorner;
                     if (hasSomeRoundCorner)
-                    {
-                        roundrect = RenderUtils.GetRoundRect(rect, ActualCornerNW, ActualCornerNE, ActualCornerSE, ActualCornerSW);
+                    {   
+
+                        roundrect = RenderUtils.GetRoundRect(p.Platform ,
+                            rect, 
+                            ActualCornerNW,
+                            ActualCornerNE, 
+                            ActualCornerSE,
+                            ActualCornerSW);
                     }
 
                     if (!p.AvoidGeometryAntialias && hasSomeRoundCorner)
