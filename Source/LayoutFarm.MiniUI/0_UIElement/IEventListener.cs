aab17d1fa3b1867f6f318a516eac07f132a0ffc9
win <wintercoredev@gmail.com>
2014-10-23 15:29:54 +0700

    7680.1

---
+++ Source/LayoutFarm.MiniUI/0_UIElement/IEventListener.cs
@@ -0,0 +1,29 @@
+﻿//2014 Apache2, WinterDev
+using System;
+using System.Collections.Generic;
+using LayoutFarm.Drawing;
+
+namespace LayoutFarm.UI
+{
+
+    public interface IEventListener
+    {
+        //--------------------------------------------------------------------------
+        void ListenKeyPressEvent(UIKeyEventArgs args);
+        void ListenKeyEvent(UIKeyEventName keyEventName, UIKeyEventArgs e);
+        bool ListenProcessDialogKey(UIKeyEventArgs args);
+        void ListenMouseEvent(UIMouseEventName mouseEventName, UIMouseEventArgs e);
+        void ListenDragEvent(UIDragEventName dragEventName, UIMouseEventArgs e);
+        void ListenFocusEvent(UIFocusEventName focusEventName, UIFocusEventArgs e);
+
+
+        //--------------------------------------------------------------------------
+        bool AcceptKeyboardFocus { get; }
+        bool NeedPreviewBubbleUp { get; }
+    }
+
+    public interface IEventPortal
+    {
+        void PortalEventKeyPress();
+    }
+}
