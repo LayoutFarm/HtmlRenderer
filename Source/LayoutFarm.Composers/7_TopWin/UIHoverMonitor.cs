258a5bac5eed96353b5c56b1d5c7711be47fbf83
win <wintercoredev@gmail.com>
2014-10-25 06:17:51 +0700

    7694.1

---
+++ Source/LayoutFarm.Composers/7_TopWin/UIHoverMonitor.cs
@@ -0,0 +1,69 @@
+﻿//2014 Apache2, WinterDev
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LayoutFarm.Drawing;
+
+namespace LayoutFarm.UI
+{
+
+
+    public class UIHoverMonitorTask : UITimerTask
+    {
+
+        int mouseMoveCounter = -1;
+        EventHandler targetEventHandler;
+        TopWindowRenderBox topwin;
+        bool isEnabled;
+        public UIHoverMonitorTask(TopWindowRenderBox topwin, EventHandler targetEventHandler)
+        {
+            this.topwin = topwin;
+            this.targetEventHandler = targetEventHandler;
+        }
+      
+        public override bool Enabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                this.isEnabled = value;
+                if (value)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public override bool IsInQueue
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public override void Reset()
+        {
+            mouseMoveCounter = -1;
+        }
+        public override void Tick()
+        {
+            mouseMoveCounter++;
+            if (mouseMoveCounter > 1)
+            {
+                targetEventHandler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+
+}
