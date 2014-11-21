54bcb546421c70e52a5939984158618af241b3f7
win <wintercoredev@gmail.com>
2014-11-21 15:22:48 +0700

    7792:  SVGPath +samples

--- Source/LayoutFarm.HtmlRenderer/4_Svg/08_Paths/SvgPathSegments.cs
+++ Source/LayoutFarm.HtmlRenderer/4_Svg/08_Paths/SvgPathSegments.cs
@@ -32,13 +32,13 @@ namespace Svg.Pathing
             get;
             set;
         }
-         
+
 
     }
 
     public class SvgPathSegMoveTo : SvgPathSeg
     {
-         
+
         public SvgPathSegMoveTo(float x, float y)
         {
             this.X = x;
@@ -51,6 +51,18 @@ namespace Svg.Pathing
         public float X { get; private set; }
         public float Y { get; private set; }
 
+        public void GetAbsolutePoints(ref PointF last, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
+
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+            }
+        }
 
 #if DEBUG
         public override string ToString()
@@ -63,7 +75,7 @@ namespace Svg.Pathing
     }
     public class SvgPathSegLineTo : SvgPathSeg
     {
-        
+
         public SvgPathSegLineTo(float x, float y)
         {
             this.X = x;
@@ -73,9 +85,20 @@ namespace Svg.Pathing
         {
             get { return SvgPathCommand.LineTo; }
         }
-        public float X { get; private set;  }
+        public float X { get; private set; }
         public float Y { get; private set; }
+        public void GetAbsolutePoints(ref PointF last, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
 
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+            }
+        }
 #if DEBUG
         public override string ToString()
         {
@@ -87,7 +110,7 @@ namespace Svg.Pathing
     }
     public class SvgPathSegLineToHorizontal : SvgPathSeg
     {
-        
+
         public SvgPathSegLineToHorizontal(float x)
         {
             this.X = x;
@@ -97,7 +120,18 @@ namespace Svg.Pathing
             get { return SvgPathCommand.HorizontalLineTo; }
         }
         public float X { get; private set; }
+        public void GetAbsolutePoints(ref PointF last, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, last.Y);
 
+            }
+            else
+            {
+                p = new PointF(this.X, last.Y);
+            }
+        }
 #if DEBUG
         public override string ToString()
         {
@@ -109,7 +143,7 @@ namespace Svg.Pathing
     }
     public class SvgPathSegLineToVertical : SvgPathSeg
     {
-         
+
         public SvgPathSegLineToVertical(float y)
         {
             this.Y = y;
@@ -120,7 +154,18 @@ namespace Svg.Pathing
             get { return SvgPathCommand.VerticalLineTo; }
         }
         public float Y { get; private set; }
+        public void GetAbsolutePoints(ref PointF last, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(last.X, this.Y + last.Y);
 
+            }
+            else
+            {
+                p = new PointF(last.X, last.Y);
+            }
+        }
 #if DEBUG
         public override string ToString()
         {
@@ -154,12 +199,52 @@ namespace Svg.Pathing
         public float X2 { get; private set; }
         public float Y2 { get; private set; }
 
-        public static PointF MakeMirrorPoint(PointF p1, PointF p2)
+        //public static PointF MakeMirrorPoint(PointF p1, PointF p2)
+        //{
+        //    //make new mirror point both x and y 
+        //    float xdiff = p2.X - p1.X; 
+        //    float ydiff = p2.Y - p1.Y; 
+        //    return new PointF(p1.X - xdiff, p1.Y - ydiff);
+        //}
+        public static PointF Reflect(PointF point, PointF mirror)
+        {   
+            float x, y, dx, dy;
+            dx = Math.Abs(mirror.X - point.X);
+            dy = Math.Abs(mirror.Y - point.Y);
+
+            if (mirror.X >= point.X)
+            {
+                x = mirror.X + dx;
+            }
+            else
+            {
+                x = mirror.X - dx;
+            }
+            if (mirror.Y >= point.Y)
+            {
+                y = mirror.Y + dy;
+            }
+            else
+            {
+                y = mirror.Y - dy;
+            }
+
+            return new PointF(x, y);
+        }
+        public void GetAbsolutePoints(ref PointF last, out PointF c1, out PointF c2, out PointF p)
         {
-            //make new mirror point both x and y
-            float xdiff = p2.X - p1.X;
-            float ydiff = p2.Y - p1.Y;
-            return new PointF(p1.X - xdiff, p1.Y - ydiff);
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
+                c1 = new PointF(this.X1 + last.X, this.Y1 + last.Y);
+                c2 = new PointF(this.X2 + last.X, this.Y2 + last.Y);
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+                c1 = new PointF(this.X1, this.Y1);
+                c2 = new PointF(this.X2, this.Y2);
+            }
         }
 #if DEBUG
         public override string ToString()
@@ -171,6 +256,103 @@ namespace Svg.Pathing
                 + this.X2 + " " + this.Y2;
         }
 #endif
+    }
+
+    public static class SvgCurveHelper
+    {
+
+        internal static void MakeBezierCurveFromArc(ref PointF start,
+            ref PointF end,
+            float rx,
+            float ry,
+            float angle,
+            SvgArcSize arcSize,
+            SvgArcSweep arcSweep,
+            out PointF[] bezier4Points)
+        {
+
+
+            double sinPhi = Math.Sin(angle * SvgPathSegArc.RAD_PER_DEG);
+            double cosPhi = Math.Cos(angle * SvgPathSegArc.RAD_PER_DEG);
+
+            double x1dash = cosPhi * (start.X - end.X) / 2.0 + sinPhi * (start.Y - end.Y) / 2.0;
+            double y1dash = -sinPhi * (start.X - end.X) / 2.0 + cosPhi * (start.Y - end.Y) / 2.0;
+
+            double root;
+            double numerator = (rx * rx * ry * ry) - (rx * rx * y1dash * y1dash) - (ry * ry * x1dash * x1dash);
+
+            if (numerator < 0.0)
+            {
+                float s = (float)Math.Sqrt(1.0 - numerator / (rx * rx * ry * ry));
+                rx *= s;
+                ry *= s;
+                root = 0.0;
+            }
+            else
+            {
+                root = ((arcSize == SvgArcSize.Large && arcSweep == SvgArcSweep.Positive)
+                    || (arcSize == SvgArcSize.Small && arcSweep == SvgArcSweep.Negative) ? -1.0 : 1.0) * Math.Sqrt(numerator / (rx * rx * y1dash * y1dash + ry * ry * x1dash * x1dash));
+            }
+
+
+            double cxdash = root * rx * y1dash / ry;
+            double cydash = -root * ry * x1dash / rx;
+
+            double cx = cosPhi * cxdash - sinPhi * cydash + (start.X + end.X) / 2.0;
+            double cy = sinPhi * cxdash + cosPhi * cydash + (start.Y + end.Y) / 2.0;
+
+            double theta1 = SvgPathSegArc.CalculateVectorAngle(1.0, 0.0, (x1dash - cxdash) / rx, (y1dash - cydash) / ry);
+            double dtheta = SvgPathSegArc.CalculateVectorAngle((x1dash - cxdash) / rx, (y1dash - cydash) / ry, (-x1dash - cxdash) / rx, (-y1dash - cydash) / ry);
+
+            if (arcSweep == SvgArcSweep.Negative && dtheta > 0)
+            {
+                dtheta -= 2.0 * Math.PI;
+            }
+            else if (arcSweep == SvgArcSweep.Positive && dtheta < 0)
+            {
+                dtheta += 2.0 * Math.PI;
+            }
+
+            int nsegments = (int)Math.Ceiling((double)Math.Abs(dtheta / (Math.PI / 2.0)));
+
+            double delta = dtheta / nsegments;
+            double t = 8.0 / 3.0 * Math.Sin(delta / 4.0) * Math.Sin(delta / 4.0) / Math.Sin(delta / 2.0);
+
+            double startX = start.X;
+            double startY = start.Y;
+
+            bezier4Points = new PointF[nsegments * 4];
+
+            int nn = 0;
+            for (int n = 0; n < nsegments; ++n)
+            {
+                double cosTheta1 = Math.Cos(theta1);
+                double sinTheta1 = Math.Sin(theta1);
+                double theta2 = theta1 + delta;
+                double cosTheta2 = Math.Cos(theta2);
+                double sinTheta2 = Math.Sin(theta2);
+
+                double endpointX = cosPhi * rx * cosTheta2 - sinPhi * ry * sinTheta2 + cx;
+                double endpointY = sinPhi * rx * cosTheta2 + cosPhi * ry * sinTheta2 + cy;
+
+                double dx1 = t * (-cosPhi * rx * sinTheta1 - sinPhi * ry * cosTheta1);
+                double dy1 = t * (-sinPhi * rx * sinTheta1 + cosPhi * ry * cosTheta1);
+
+                double dxe = t * (cosPhi * rx * sinTheta2 + sinPhi * ry * cosTheta2);
+                double dye = t * (sinPhi * rx * sinTheta2 - cosPhi * ry * cosTheta2);
+
+                bezier4Points[nn] = new PointF((float)startX, (float)startY);
+                bezier4Points[nn + 1] = new PointF((float)(startX + dx1), (float)(startY + dy1));
+                bezier4Points[nn + 2] = new PointF((float)(endpointX + dxe), (float)(endpointY + dye));
+                bezier4Points[nn + 3] = new PointF((float)endpointX, (float)endpointY);
+
+                nn += 4;
+
+                theta1 = theta2;
+                startX = (float)endpointX;
+                startY = (float)endpointY;
+            }
+        }
     }
     public class SvgPathSegCurveToCubicSmooth : SvgPathSeg
     {
@@ -190,6 +372,19 @@ namespace Svg.Pathing
         public float X2 { get; private set; }
         public float Y2 { get; private set; }
 
+        public void GetAbsolutePoints(ref PointF last, out PointF c2, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
+                c2 = new PointF(this.X2 + last.X, this.Y2 + last.Y);
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+                c2 = new PointF(this.X2, this.Y2);
+            }
+        }
 #if DEBUG
         public override string ToString()
         {
@@ -222,8 +417,23 @@ namespace Svg.Pathing
         public PointF ControlPoint
         {
             get { return new PointF(this.X1, this.Y1); }
-            
+
         }
+        public void GetAbsolutePoints(ref PointF last, out PointF c1, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
+                c1 = new PointF(this.X1 + last.X, this.Y1 + last.Y);
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+                c1 = new PointF(this.X1, this.Y1);
+            }
+        }
+
+
         public static void GetControlPoints(PointF start, PointF controlPoint, PointF endPoint, out PointF control1, out PointF control2)
         {
             float x1 = start.X + (controlPoint.X - start.X) * 2 / 3;
@@ -231,7 +441,7 @@ namespace Svg.Pathing
             float x2 = controlPoint.X + (endPoint.X - controlPoint.X) / 3;
             float y2 = controlPoint.Y + (endPoint.Y - controlPoint.Y) / 3;
 
-            control1 = new PointF(x1, y2);
+            control1 = new PointF(x1, y1);
             control2 = new PointF(x2, y2);
         }
 #if DEBUG
@@ -257,6 +467,22 @@ namespace Svg.Pathing
         }
         public float X { get; private set; }
         public float Y { get; private set; }
+
+
+        public void GetAbsolutePoints(ref PointF last, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
+
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+
+            }
+        }
+
 #if DEBUG
         public override string ToString()
         {
@@ -297,6 +523,20 @@ namespace Svg.Pathing
         public SvgArcSize LargeArgFlag { get; private set; }
         public SvgArcSweep SweepFlag { get; private set; }
 
+
+        public void GetAbsolutePoints(ref PointF last, out PointF p)
+        {
+            if (this.IsRelative)
+            {
+                p = new PointF(this.X + last.X, this.Y + last.Y);
+
+            }
+            else
+            {
+                p = new PointF(this.X, this.Y);
+
+            }
+        }
         public static double CalculateVectorAngle(double ux, double uy, double vx, double vy)
         {
             double ta = Math.Atan2(uy, ux);
