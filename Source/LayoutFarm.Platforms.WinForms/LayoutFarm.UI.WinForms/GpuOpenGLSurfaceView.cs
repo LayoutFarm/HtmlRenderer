b9f57c6b5c92fd49dad80f4486e9b1d6b2680e45
win <wintercoredev@gmail.com>
2014-12-25 21:08:40 +0700

    7922: add bridge to GpuOpenGLSurfaceView
    developing...

--- Source/LayoutFarm.Platforms.WinForms/LayoutFarm.UI.WinForms/GpuOpenGLSurfaceView.cs
+++ Source/LayoutFarm.Platforms.WinForms/LayoutFarm.UI.WinForms/GpuOpenGLSurfaceView.cs
@@ -60,14 +60,12 @@ namespace LayoutFarm.UI.WinForms
                         (float)clearColor.A / 255f);
                 }
             }
-        }
-
-        protected override void OnMouseDown(MouseEventArgs e)
-        {
-            base.OnMouseDown(e);
-        }
+        } 
         protected override void OnPaint(PaintEventArgs e)
         {
+            //this.winBridge.PaintMe(e);
+            //base.OnPaint(e);
+
             base.OnPaint(e);
             //------------------------------------------
             if (!this.DesignMode)
@@ -101,5 +99,88 @@ namespace LayoutFarm.UI.WinForms
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
         }
+
+        //-----------------------------
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            if (this.winBridge != null)
+            {
+                this.winBridge.UpdateCanvasViewportSize(this.Width, this.Height);
+            }
+            base.OnSizeChanged(e);
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            this.winBridge.HandleMouseEnterToViewport();
+            base.OnMouseEnter(e);
+        }
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            this.winBridge.HandleMouseLeaveFromViewport();
+            base.OnMouseLeave(e);
+        }
+        //-----------------------------------------------------------------------------
+        protected override void OnGotFocus(EventArgs e)
+        {
+            this.winBridge.HandleGotFocus(e);
+            base.OnGotFocus(e);
+
+        }
+        protected override void OnLostFocus(EventArgs e)
+        {
+            this.winBridge.HandleGotFocus(e);
+            base.OnLostFocus(e);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            this.winBridge.HandleMouseDown(e);
+            base.OnMouseDown(e);
+
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            this.winBridge.HandleMouseMove(e);
+            base.OnMouseMove(e);
+
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            this.winBridge.HandleMouseUp(e);
+            base.OnMouseUp(e);
+
+        }
+         
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            this.winBridge.HandleMouseWheel(e);
+            base.OnMouseWheel(e);
+        }
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            this.winBridge.HandleKeyDown(e);
+            base.OnKeyDown(e);
+        }
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            this.winBridge.HandleKeyUp(e);
+            base.OnKeyUp(e);
+        }
+        protected override void OnKeyPress(System.Windows.Forms.KeyPressEventArgs e)
+        {
+            this.winBridge.HandleKeyPress(e);
+            return;
+        }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (this.winBridge.ProcessDialogKey(keyData))
+            {
+                return true;
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+
     }
 }
--- Source/LayoutFarm.Platforms.WinForms/LayoutFarm.UI.WinForms/UISurfaceViewportControl.cs
+++ Source/LayoutFarm.Platforms.WinForms/LayoutFarm.UI.WinForms/UISurfaceViewportControl.cs
@@ -26,7 +26,7 @@ namespace LayoutFarm.UI.WinForms
             InnerViewportKind innerViewportKind)
         {
             //test*** force  to use gl
-            innerViewportKind = InnerViewportKind.GL;
+            //innerViewportKind = InnerViewportKind.GL;
             //---------------------------------------- 
             //1.
             this.wintop = wintop;
