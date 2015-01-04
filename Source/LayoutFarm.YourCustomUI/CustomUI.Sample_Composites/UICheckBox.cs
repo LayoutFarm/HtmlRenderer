eb85f79adddc0fb3dd66b5b987b059ab46c09cda
win <wintercoredev@gmail.com>
2015-01-04 13:24:19 +0700

    7986: UICheckBox

---
+++ Source/LayoutFarm.YourCustomUI/CustomUI.Sample_Composites/UICheckBox.cs
@@ -0,0 +1,94 @@
+﻿//2014 Apache2, WinterDev
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+using LayoutFarm.Drawing;
+using LayoutFarm.Text;
+using LayoutFarm.UI;
+
+namespace LayoutFarm.SampleControls
+{
+    public interface ICheckable
+    {
+        bool Checked { get; }
+
+    }
+
+    public class UICheckBox : UIPanel, ICheckable
+    {
+        //check icon
+
+        UIImageBox imageBox;
+        bool isChecked;
+        public UICheckBox(int w, int h)
+            : base(w, h)
+        {
+
+        }
+        public override RenderElement GetPrimaryRenderElement(RootGraphic rootgfx)
+        {
+            if (!this.HasReadyRenderElement)
+            {
+                //first time
+                RenderElement baseRenderElement = base.GetPrimaryRenderElement(rootgfx);
+                imageBox = new UIImageBox(16, 16);
+
+                if (this.isChecked)
+                {
+                    imageBox.Image = ResImageList.GetImage(ImageName.CheckBoxChecked);
+                }
+                else
+                {
+                    imageBox.Image = ResImageList.GetImage(ImageName.CheckBoxUnChecked);
+                }
+
+                imageBox.MouseDown += (s, e) =>
+                {
+                    //toggle checked/unchecked
+                    this.Checked = !this.Checked;
+
+                };
+                this.AddChildBox(imageBox);
+                return baseRenderElement;
+            }
+            else
+            {
+                return base.GetPrimaryRenderElement(rootgfx);
+            }
+
+        }
+        public bool Checked
+        {
+            get { return this.isChecked; }
+            set
+            {
+                if (value != this.isChecked)
+                {
+                    this.isChecked = value;
+                    //check check image too!
+                    if (this.isChecked)
+                    {
+                        imageBox.Image = ResImageList.GetImage(ImageName.CheckBoxChecked);
+                    }
+                    else
+                    {
+                        imageBox.Image = ResImageList.GetImage(ImageName.CheckBoxUnChecked);
+                    }
+
+                    if (value && this.WhenChecked != null)
+                    {
+                        this.WhenChecked(this, EventArgs.Empty);
+                    }
+                }
+            }
+        }
+
+        public event EventHandler WhenChecked;
+
+
+    }
+
+
+}
