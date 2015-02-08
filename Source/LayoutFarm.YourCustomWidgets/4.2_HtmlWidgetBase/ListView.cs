ac3e6d32ccbfb1de0277382150117c29c8071469
win <wintercoredev@gmail.com>
2015-02-08 19:12:58 +0700

    8313: ListView, developing ...

--- Source/LayoutFarm.YourCustomWidgets/4.2_HtmlWidgetBase/ListView.cs
+++ Source/LayoutFarm.YourCustomWidgets/4.2_HtmlWidgetBase/ListView.cs
@@ -8,101 +8,44 @@ using PixelFarm.Drawing;
 using LayoutFarm.UI;
 using LayoutFarm.RenderBoxes;
 using LayoutFarm.CustomWidgets;
+using LayoutFarm.HtmlBoxes;
+
 namespace LayoutFarm.HtmlWidgets
 {
-    public class ListView : UIBox
+    public class ListView : LightHtmlWidgetBase
     {
-        //composite          
-        CustomRenderBox primElement;//background
+        //composite           
         Color backColor = Color.LightGray;
         int viewportX, viewportY;
         List<UICollection> layers = new List<UICollection>(1);
         List<ListItem> items = new List<ListItem>();
         int selectedIndex = -1;//default = no selection
-        Panel panel;
-        public ListView(int width, int height)
-            : base(width, height)
-        {
-            UICollection plainLayer = new UICollection(this);
-            //panel for listview items
-            this.panel = new Panel(width, height);
-            this.panel.PanelLayoutKind = PanelLayoutKind.VerticalStack;
-            panel.BackColor = Color.LightGray;
-            plainLayer.AddUI(panel);
-            this.layers.Add(plainLayer);
-        }
-        protected override bool HasReadyRenderElement
-        {
-            get { return this.primElement != null; }
-        }
-        public override RenderElement CurrentPrimaryRenderElement
-        {
-            get { return this.primElement; }
-        }
-        public Color BackColor
-        {
-            get { return this.backColor; }
-            set
-            {
-                this.backColor = value;
-                if (HasReadyRenderElement)
-                {
-                    this.primElement.BackColor = value;
-                }
-            }
-        }
-        public override RenderElement GetPrimaryRenderElement(RootGraphic rootgfx)
-        {
-            if (primElement == null)
-            {
-                var renderE = new CustomRenderBox(rootgfx, this.Width, this.Height);
 
-                renderE.SetLocation(this.Left, this.Top);
-                renderE.BackColor = backColor;
-                renderE.SetController(this);
-                renderE.HasSpecificSize = true;
-
-                //------------------------------------------------
-                //create visual layer
-                renderE.Layers = new VisualLayerCollection();
-                int layerCount = this.layers.Count;
-                for (int m = 0; m < layerCount; ++m)
-                {
-                    UICollection plain = (UICollection)this.layers[m];
-                    var groundLayer = new PlainLayer(renderE);
-                    renderE.Layers.AddLayer(groundLayer);
-                    renderE.SetViewport(this.viewportX, this.viewportY);
-                    //---------------------------------
-                    int j = plain.Count;
-                    for (int i = 0; i < j; ++i)
-                    {
-                        groundLayer.AddUI(plain.GetElement(i));
-                    }
-                }
-
-                //---------------------------------
-                renderE.SetVisible(this.Visible);
-                primElement = renderE;
-            }
-            return primElement;
-        }
-
-        protected override void OnContentLayout()
+        public ListView(HtmlHost htmlhost, int w, int h)
+            : base(htmlhost, w, h)
         {
-            panel.PerformContentLayout();
+
         }
-        public override bool NeedContentLayout
-        {
-            get
-            {
-                return this.panel.NeedContentLayout;
-            }
-        }
+        //protected override void OnContentLayout()
+        //{
+        //    panel.PerformContentLayout();
+        //}
+        //public override bool NeedContentLayout
+        //{
+        //    get
+        //    {
+        //        return this.panel.NeedContentLayout;
+        //    }
+        //}
         //----------------------------------------------------
+        protected override Composers.FragmentHtmlDocument CreatePresentationDom()
+        {
+            throw new NotImplementedException();
+        }
         public void AddItem(ListItem ui)
         {
             items.Add(ui);
-            panel.AddChildBox(ui);
+
         }
         public int ItemCount
         {
@@ -111,7 +54,6 @@ namespace LayoutFarm.HtmlWidgets
         public void RemoveAt(int index)
         {
             var item = items[index];
-            panel.RemoveChildBox(item);
             items.RemoveAt(index);
 
         }
@@ -129,13 +71,12 @@ namespace LayoutFarm.HtmlWidgets
         public void Remove(ListItem item)
         {
             items.Remove(item);
-            panel.RemoveChildBox(item);
+
         }
         public void ClearItems()
         {
             this.selectedIndex = -1;
             this.items.Clear();
-            this.panel.ClearItems();
         }
         //----------------------------------------------------
 
@@ -174,93 +115,50 @@ namespace LayoutFarm.HtmlWidgets
                 }
             }
         }
-        //----------------------------------------------------
-        protected override void OnMouseDown(UIMouseEventArgs e)
-        {
-            if (this.MouseDown != null)
-            {
-                this.MouseDown(this, e);
-            }
-        }
-        protected override void OnMouseUp(UIMouseEventArgs e)
-        {
-            if (this.MouseUp != null)
-            {
-                MouseUp(this, e);
-            }
-            base.OnMouseUp(e);
-        }
-
-
-        public override int ViewportX
+        public int ViewportX
         {
             get { return this.viewportX; }
-
         }
-        public override int ViewportY
+        public int ViewportY
         {
             get { return this.viewportY; }
-
         }
-        public override void SetViewport(int x, int y)
+
+        //public override int ViewportX
+        //{
+        //    get { return this.viewportX; }
+
+        //}
+        //public override int ViewportY
+        //{
+        //    get { return this.viewportY; }
+
+        //}
+        public void SetViewport(int x, int y)
         {
             this.viewportX = x;
             this.viewportY = y;
-            if (this.HasReadyRenderElement)
-            {
-                this.panel.SetViewport(x, y);
-            }
+            //set viewport 
+            //if (this.HasReadyRenderElement)
+            //{
+            //    this.panel.SetViewport(x, y);
+            //}
         }
-        //----------------------------------------------------
-
-        public event EventHandler<UIMouseEventArgs> MouseDown;
-        public event EventHandler<UIMouseEventArgs> MouseUp;
-
 
     }
-
-
-    public class ListItem : UIBox
+    public class ListItem
     {
-        CustomContainerRenderBox primElement;
+
         CustomTextRun listItemText;
 
         string itemText;
         Color backColor;
+        int width;
+        int height;
         public ListItem(int width, int height)
-            : base(width, height)
         {
-        }
-        public override RenderElement CurrentPrimaryRenderElement
-        {
-            get { return this.primElement; }
-        }
-        protected override bool HasReadyRenderElement
-        {
-            get { return primElement != null; }
-        }
-        public override RenderElement GetPrimaryRenderElement(RootGraphic rootgfx)
-        {
-            if (primElement == null)
-            {
-                //1.
-                var element = new CustomContainerRenderBox(rootgfx, this.Width, this.Height);
-
-                element.SetLocation(this.Left, this.Top);
-                element.BackColor = this.backColor;
-
-
-                listItemText = new CustomTextRun(rootgfx, 200, this.Height);
-                element.AddChildBox(listItemText);
-
-                if (this.itemText != null)
-                {
-                    listItemText.Text = this.itemText;
-                }
-
-                this.primElement = element;
-            }
-            return primElement;
+            this.width = width;
+            this.height = height;
         }
         public Color BackColor
         {
@@ -268,10 +166,6 @@ namespace LayoutFarm.HtmlWidgets
             set
             {
                 this.backColor = value;
-                if (HasReadyRenderElement)
-                {
-                    this.primElement.BackColor = value;
-                }
             }
         }
         public string Text
@@ -289,7 +183,8 @@ namespace LayoutFarm.HtmlWidgets
         //----------------- 
         public void AddChild(RenderElement renderE)
         {
-            primElement.AddChildBox(renderE);
+            //add content to list item
+
         }
     }
 
---
+++ Source/Test3_MixHtml/Demo5/5.7_Demo_ListView.cs
@@ -0,0 +1,75 @@
+﻿// 2015,2014 ,Apache2, WinterDev
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using PixelFarm.Drawing;
+using LayoutFarm.UI;
+
+namespace LayoutFarm.WebWidgets
+{
+    [DemoNote("5.7 ListView")]
+    class Demo_ListView : DemoBase
+    {
+
+        LayoutFarm.ContentManagers.ImageContentManager imageContentMan = new ContentManagers.ImageContentManager();
+        LayoutFarm.HtmlBoxes.HtmlHost htmlHost;
+        protected override void OnStartDemo(SampleViewport viewport)
+        {
+            imageContentMan.ImageLoadingRequest += (s, e) =>
+            {
+                e.SetResultImage(LoadBitmap(e.ImagSource));
+            };
+
+            htmlHost = HtmlHostCreatorHelper.CreateHtmlHost(viewport,
+              (s, e) => this.imageContentMan.AddRequestImage(e.ImageBinder),
+              (s, e) => { });
+            //-------------------------------------------------------------------
+             
+            int boxX = 0;
+            for (int i = 0; i < 1; ++i)
+            {
+                var hingeBox = new LayoutFarm.HtmlWidgets.HingeBox(htmlHost, 100, 30);
+                hingeBox.SetLocation(boxX, 20);
+                boxX += 100 + 2;
+                viewport.AddContent(hingeBox);
+            }
+        }
+        LayoutFarm.HtmlWidgets.HingeBox CreateHingeBox(int w, int h)
+        {
+            var hingeBox = new LayoutFarm.HtmlWidgets.HingeBox(htmlHost, 100, 30); 
+            //1. set land part detail
+            //2. set float part detail
+
+            return hingeBox;
+        }
+
+        static void SetupImageList()
+        {
+            if (!LayoutFarm.CustomWidgets.ResImageList.HasImages)
+            {
+                //set imagelists
+                var imgdic = new Dictionary<CustomWidgets.ImageName, Image>();
+                imgdic[CustomWidgets.ImageName.CheckBoxUnChecked] = LoadBitmap("../../Demo/arrow_close.png");
+                imgdic[CustomWidgets.ImageName.CheckBoxChecked] = LoadBitmap("../../Demo/arrow_open.png");
+                LayoutFarm.CustomWidgets.ResImageList.SetImageList(imgdic);
+            }
+        }
+        static Bitmap LoadBitmap(string filename)
+        {
+            System.Drawing.Bitmap gdiBmp = new System.Drawing.Bitmap(filename);
+            Bitmap bmp = new Bitmap(gdiBmp.Width, gdiBmp.Height, gdiBmp);
+            return bmp;
+        }
+        static ImageBinder LoadImage(string filename)
+        {
+            System.Drawing.Bitmap gdiBmp = new System.Drawing.Bitmap(filename);
+            Bitmap bmp = new Bitmap(gdiBmp.Width, gdiBmp.Height, gdiBmp);
+
+            ImageBinder binder = new ClientImageBinder(null);
+            binder.SetImage(bmp);
+            binder.State = ImageBinderState.Loaded;
+            return binder;
+        }
+    }
+}
