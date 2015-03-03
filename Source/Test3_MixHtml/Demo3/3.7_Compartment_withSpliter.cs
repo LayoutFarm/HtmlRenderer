62542d33f26d812c5b16852e364768ae11e7406d
win <wintercoredev@gmail.com>
2015-03-03 18:51:51 +0700

    8424.1: Demo_CompartmentWithSpliter

---
+++ Source/Test3_MixHtml/Demo3/3.7_Compartment_withSpliter.cs
@@ -0,0 +1,44 @@
+﻿// 2015,2014 ,Apache2, WinterDev
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using PixelFarm.Drawing;
+using LayoutFarm.UI;
+using LayoutFarm.CustomWidgets;
+using LayoutFarm.RenderBoxes;
+namespace LayoutFarm
+{
+    [DemoNote("3.7 Demo_CompartmentWithSpliter")]
+    class Demo_CompartmentWithSpliter : DemoBase
+    {
+
+        NinespaceBox ninespaceBox;
+        protected override void OnStartDemo(SampleViewport viewport)
+        {
+
+            //--------------------------------
+            {
+                //background element
+                var bgbox = new LayoutFarm.CustomWidgets.EaseBox(800, 600);
+                bgbox.BackColor = Color.White;
+                bgbox.SetLocation(0, 0);
+                SetupBackgroundProperties(bgbox);
+                viewport.AddContent(bgbox);
+            }
+            //--------------------------------
+            //ninespace compartment
+            ninespaceBox = new NinespaceBox(800, 600);
+            ninespaceBox.ShowGrippers = true;
+            viewport.AddContent(ninespaceBox);
+            ninespaceBox.SetSize(800, 600);
+        }
+        void SetupBackgroundProperties(LayoutFarm.CustomWidgets.EaseBox backgroundBox)
+        {
+
+        }
+
+   
+
+    }
+}
