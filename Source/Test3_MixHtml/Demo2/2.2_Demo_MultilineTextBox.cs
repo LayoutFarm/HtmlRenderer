5c7f7f997d7555e7988f0155b67c01c446a48c62
win <wintercoredev@gmail.com>
2015-03-22 01:05:55 +0700

    8490: fixed span's padding

--- Source/Test3_MixHtml/Demo2/2.2_Demo_MultilineTextBox.cs
+++ Source/Test3_MixHtml/Demo2/2.2_Demo_MultilineTextBox.cs
@@ -14,13 +14,13 @@ namespace LayoutFarm
         protected override void OnStartDemo(SampleViewport viewport)
         {
 
-            var textbox1 = new LayoutFarm.CustomWidgets.TextBox(400, 500, true);
+            var textbox1 = new LayoutFarm.CustomWidgets.TextBox(400, 100, true);
             var style1 = new Text.TextSpanStyle();
             style1.FontInfo = viewport.P.GetFont("tahoma", 10, PixelFarm.Drawing.FontStyle.Regular);
             textbox1.DefaultSpanStyle = style1;
             viewport.AddContent(textbox1);
              
-            var textbox2 = new LayoutFarm.CustomWidgets.TextBox(400, 100, true);
+            var textbox2 = new LayoutFarm.CustomWidgets.TextBox(400, 500, true);
             textbox2.SetLocation(20, 120);
             viewport.AddContent(textbox2);
             textbox2.Text = "Hello World!";
