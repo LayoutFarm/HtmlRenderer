aee64acea66a10a6b7522f9861063cdd42d6cb80
win <wintercoredev@gmail.com>
2015-04-25 13:25:27 +0700

    8880: lex samples

--- Source/Test2_WebDomParsers/Form1.Designer.cs
+++ Source/Test2_WebDomParsers/Form1.Designer.cs
@@ -29,26 +29,56 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(239, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 48);
+            this.button1.Size = new System.Drawing.Size(121, 28);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Parse";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 46);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(221, 401);
+            this.treeView1.TabIndex = 2;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Location = new System.Drawing.Point(638, 46);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(401, 401);
+            this.treeView2.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(239, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(393, 401);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(686, 369);
+            this.ClientSize = new System.Drawing.Size(1051, 564);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +86,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
