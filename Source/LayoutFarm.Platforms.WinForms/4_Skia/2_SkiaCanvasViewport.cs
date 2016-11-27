20abfb1add4a9461c837ab6c469a5b46ed26132b
win <wintercoredev@gmail.com>
2016-11-27 14:13:01 +0700

    9135: cached buffer

--- Source/LayoutFarm.Platforms.WinForms/4_Skia/2_SkiaCanvasViewport.cs
+++ Source/LayoutFarm.Platforms.WinForms/4_Skia/2_SkiaCanvasViewport.cs
@@ -10,35 +10,44 @@ namespace LayoutFarm.UI.Skia
 {
     class SkiaCanvasViewport : CanvasViewport
     {
-        
+
         PixelFarm.Drawing.Skia.MySkiaCanvas mySkCanvas;
         int internalSizeW = 800;
         int internalSizwH = 600;
-
+        //-
+        //TODO: review here again
+        System.Drawing.Bitmap tmpBmp;
+        byte[] tmpBuffer;
         public SkiaCanvasViewport(RootGraphic rootgfx,
             Size viewportSize, int cachedPageNum)
             : base(rootgfx, viewportSize, cachedPageNum)
         {
-            //quadPages = new QuadPages(cachedPageNum, viewportSize.Width, viewportSize.Height * 2);
+
             this.CalculateCanvasPages();
-
             mySkCanvas = new PixelFarm.Drawing.Skia.MySkiaCanvas(0, 0, 0, 0, internalSizeW, internalSizwH);
-             
+            //TODO: review performance here
+            //review how to move data from unmanged(skia) to unmanaged(hdc's bitmap)
+            tmpBmp = new System.Drawing.Bitmap(internalSizeW, internalSizwH);
+            var bmpdata = tmpBmp.LockBits(
+                new System.Drawing.Rectangle(0, 0, internalSizeW, internalSizwH),
+                 System.Drawing.Imaging.ImageLockMode.ReadOnly, tmpBmp.PixelFormat);
+            tmpBuffer = new byte[bmpdata.Stride * bmpdata.Height];
+            tmpBmp.UnlockBits(bmpdata);
         }
-       
-
-
-        //static int dbugCount = 0;
         protected override void OnClosing()
         {
-            //if (quadPages != null)
-            //{
-            //    quadPages.Dispose();
-            //    quadPages = null;
-            //}
+            if (tmpBuffer != null)
+            {
+                tmpBuffer = null;
+            }
+            if (tmpBmp != null)
+            {
+                tmpBmp.Dispose();
+                tmpBmp = null;
+            }
+
             base.OnClosing();
         }
-
         public override void CanvasInvlidateArea(Rectangle r)
         {
             //quadPages.CanvasInvalidate(r);
@@ -61,24 +70,7 @@ namespace LayoutFarm.UI.Skia
             //quadPages.CalculateCanvasPages(this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
             this.FullMode = true;
         }
-        //// get the bitmap
-        //CreateBitmap();
-        //var data = bitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
 
-        //// create the surface
-        //var info = new SKImageInfo(Width, Height, SKImageInfo.PlatformColorType, SKAlphaType.Premul);
-        //using (var surface = SKSurface.Create(info, data.Scan0, data.Stride))
-        //{
-        //    // start drawing
-        //    OnPaintSurface(new SKPaintSurfaceEventArgs(surface, info));
-
-        //    surface.Canvas.Flush();
-        //}
-
-        //// write the bitmap to the graphics
-        //bitmap.UnlockBits(data);
-        //e.Graphics.DrawImage(bitmap, 0, 0);
-         
         static void UpdateAllArea(PixelFarm.Drawing.Skia.MySkiaCanvas mycanvas, IRenderElement topWindowRenderBox)
         {
             mycanvas.OffsetCanvasOrigin(-mycanvas.Left, -mycanvas.Top);
@@ -102,76 +94,42 @@ namespace LayoutFarm.UI.Skia
             this.rootGraphics.dbug_drawLevel = 0;
 #endif
 
-            //CreateBitmap();
-
-            //var data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, Width, Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bitmap.PixelFormat);
-
-            // create the surface
-            //var info = new SkiaSharp.SKImageInfo(Width, Height, SkiaSharp.SKImageInfo.PlatformColorType, SkiaSharp.SKAlphaType.Premul);
-            //SkiaSharp.SKBitmap bmp1 = new SkiaSharp.SKBitmap(800, 600);
-            //SkiaSharp.SKCanvas ss = new SkiaSharp.SKCanvas(bmp1);
+            //1. clear sk surface
             mySkCanvas.ClearSurface(PixelFarm.Drawing.Color.White);
-
+            //2. render to the surface
             UpdateAllArea(mySkCanvas, rootGraphics.TopWindowRenderBox);
+            //3. copy bitmap buffer from the surface and render to final hdc
+
+            //-----------------------------------------------
+            //TODO: review performance here 
+            //we should copy from unmanaged (skia bitmap) 
+            //and write to unmanaged (hdc'bitmap)
+
             SkiaSharp.SKBitmap backBmp = mySkCanvas.BackBmp;
-            // backBmp.LockPixels();
+            backBmp.LockPixels();
             IntPtr h = backBmp.GetPixels();
+
             int w1 = backBmp.Width;
             int h1 = backBmp.Height;
             int stride = backBmp.RowBytes;
 
-
-            byte[] buffer1 = new byte[stride * h1];
             System.Runtime.InteropServices.Marshal.Copy(
-                h, buffer1, 0, buffer1.Length
+                h, tmpBuffer, 0, tmpBuffer.Length
                 );
-            // backBmp.UnlockPixels();
+            backBmp.UnlockPixels();
 
-            //
-            System.Drawing.Bitmap bmp1 = new System.Drawing.Bitmap(w1, h1);
-            var bmpdata = bmp1.LockBits(new System.Drawing.Rectangle(0, 0, w1, h1), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            System.Runtime.InteropServices.Marshal.Copy(buffer1, 0, bmpdata.Scan0, buffer1.Length);
-            bmp1.UnlockBits(bmpdata);
-            //if (RenderBoxBase.debugBreaK1)
-            //{
-            //    bmp1.Save("d:\\WImageTest\\t02.png");
-            //}
 
-            //using (var surface = SkiaSharp.SKSurface.Create(info, data.Scan0, data.Stride))
-            //{
-            //    // start drawing
-            //    // OnPaintSurface(new SKPaintSurfaceEventArgs(surface, info));
-            //    UpdateAllArea(mySkCanvas, rootGraphics.TopWindowRenderBox);
-            //    surface.Canvas.Flush();
-            //}
+            var bmpdata = tmpBmp.LockBits(new System.Drawing.Rectangle(0, 0, w1, h1),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            System.Runtime.InteropServices.Marshal.Copy(tmpBuffer, 0, bmpdata.Scan0, tmpBuffer.Length);
+            tmpBmp.UnlockBits(bmpdata);
 
-            // write the bitmap to the graphics
-            //bitmap.UnlockBits(data);
-
-            //using (System.Drawing.Graphics g2 = ViewControl.CreateGraphics())
             using (System.Drawing.Graphics g2 = System.Drawing.Graphics.FromHdc(hdc))
             {
-                //   bitmap.Save("d:\\WImageTest\\t01.png");
-                g2.DrawImage(bmp1, 0, 0);
+                g2.DrawImage(tmpBmp, 0, 0);
             }
-
-
-            //e.Graphics.DrawImage(bitmap, 0, 0);
-
-            //if (this.FullMode)
-            //{
-            //    //quadPages.RenderToOutputWindowFullMode(
-            //    //    rootGraphics.TopWindowRenderBox, hdc,
-            //    //    this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
-            //}
-            //else
-            //{
-            //    //temp to full mode
-            //    //quadPages.RenderToOutputWindowFullMode(rootGraphics.TopWindowRenderBox, hdc, this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
-            //    //quadPages.RenderToOutputWindowPartialMode(
-            //    //   rootGraphics.TopWindowRenderBox, hdc,
-            //    //   this.ViewportX, this.ViewportY, this.ViewportWidth, this.ViewportHeight);
-            //}
+            //-----------------------------------------------------------------------------
             this.rootGraphics.IsInRenderPhase = false;
 #if DEBUG
 
