56fcf1aaa4596551d405c27b9fbb7dee82a2bb03
win <wintercoredev@gmail.com>
2017-02-12 13:41:59 +0700

    9163.2

--- Source/LayoutFarm.Platforms.WinNeutral/3_OpenGL/3_MyTopWindowBridgeOpenGL.cs
+++ Source/LayoutFarm.Platforms.WinNeutral/3_OpenGL/3_MyTopWindowBridgeOpenGL.cs
@@ -1,9 +1,6 @@
 ﻿//Apache2, 2014-2017, WinterDev
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
+ 
 using PixelFarm.Drawing;
 using PixelFarm.Forms;
 using LayoutFarm.UI.WinNeutral;
