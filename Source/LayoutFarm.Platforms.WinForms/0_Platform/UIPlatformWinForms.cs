dcc4aa48f8629c9d8765e2afbe352f30a7b3f43e
win <wintercoredev@gmail.com>
2017-03-01 04:16:48 +0700

    9181: 2 UI platforms

---
+++ Source/LayoutFarm.Platforms.WinForms/0_Platform/UIPlatformWinForms.cs
@@ -0,0 +1,32 @@
+﻿//Apache2, 2014-2017, WinterDev
+
+namespace LayoutFarm.UI
+{
+    public class UIPlatformWinForm : UIPlatform
+    {
+        private UIPlatformWinForm()
+        {
+
+            LayoutFarm.UI.Clipboard.SetUIPlatform(this);
+
+        }
+        public override UITimer CreateUITimer()
+        {
+            return new MyUITimer();
+        }
+        public override void ClearClipboardData()
+        {
+            System.Windows.Forms.Clipboard.Clear();
+        }
+        public override string GetClipboardData()
+        {
+            return System.Windows.Forms.Clipboard.GetText();
+        }
+        public override void SetClipboardData(string textData)
+        {
+            System.Windows.Forms.Clipboard.SetText(textData);
+        } 
+        public readonly static UIPlatformWinForm platform = new UIPlatformWinForm();
+
+    }
+}
---
+++ Source/LayoutFarm.Platforms.WinNeutral/0_Platform/UIPlatformWinNeutral.cs
@@ -0,0 +1,30 @@
+﻿//Apache2, 2014-2017, WinterDev
+
+namespace LayoutFarm.UI
+{
+    public class UIPlatformWinNeutral : UIPlatform
+    {
+        private UIPlatformWinNeutral()
+        {
+            LayoutFarm.UI.Clipboard.SetUIPlatform(this);
+        }
+        public override UITimer CreateUITimer()
+        {
+            return new MyUITimer();
+        }
+        public override void ClearClipboardData()
+        {
+            throw new System.NotSupportedException();
+        }
+        public override string GetClipboardData()
+        {
+            throw new System.NotSupportedException();
+        }
+        public override void SetClipboardData(string textData)
+        {
+            throw new System.NotSupportedException();
+        }
+
+        public static readonly UIPlatformWinNeutral platform = new UIPlatformWinNeutral();
+    }
+}
