94dc9972454551e542876f72708cdb8ad9486289
win <wintercoredev@gmail.com>
2017-03-07 15:55:19 +0700

    9213: move YourImplementation

--- Source/Deps/YourImplementation.PixelFarm/Bootstrap/BootStrapWinGdi.cs
+++ Source/Deps/YourImplementation.PixelFarm/Bootstrap/BootStrapWinGdi.cs
@@ -1,13 +1,14 @@
 ﻿//MIT, 2017, WinterDev
-
+using System;
 using PixelFarm.Drawing;
 using PixelFarm.Drawing.Fonts;
 
-namespace YourImplementation
+namespace YourImplementation 
 {
     public static class BootStrapWinGdi
     {
-        public static readonly IFontLoader myFontLoader = WindowsFontLoader.Default;
+        public static readonly IFontLoader myFontLoader = new WindowsFontLoader();
     }
+  
 
 }
