cba11729a81e6b040f79ef6794fc3420fe58cd72
win <wintercoredev@gmail.com>
2017-03-19 12:55:10 +0700

    9246: add Test7_Win test project

---
+++ Source/Test7_Win/Helper/SampleViewportExtension.cs
@@ -0,0 +1,15 @@
+﻿//Apache2, 2014-2017, WinterDev
+
+using LayoutFarm.UI;
+namespace LayoutFarm
+{
+    public static class SampleViewportExtension
+    {
+        public static void AddContent(this SampleViewport viewport, UIElement ui)
+        {
+            viewport.ViewportControl.AddContent(
+                ui.GetPrimaryRenderElement(viewport.ViewportControl.RootGfx),
+                ui);
+        }
+    }
+}
