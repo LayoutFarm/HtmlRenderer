41efa817be54a3694ab73cfe4678d9db73e39757
win <wintercoredev@gmail.com>
2017-10-09 17:16:38 +0700

    9266: temp1, merge tool

--- Source/BuildMergeProject/Form1.Designer.cs
+++ Source/BuildMergeProject/Form1.Designer.cs
@@ -29,8 +29,15 @@
         private void InitializeComponent()
         {
             this.cmdBuildHtmlRenderOne = new System.Windows.Forms.Button();
-            this.cmdBuild_HtmlRendererOnePortable = new System.Windows.Forms.Button();
-            this.cmdCopyNativeResources = new System.Windows.Forms.Button();
+            //this.cmdCopyNativeResources = new System.Windows.Forms.Button();
+            this.cmdBuildSelectedMergePro = new System.Windows.Forms.Button();
+            this.lstAsmReferenceList = new System.Windows.Forms.ListBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.lstPreset = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.cmdReadSln = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cmdBuildHtmlRenderOne
@@ -43,44 +50,123 @@
             this.cmdBuildHtmlRenderOne.UseVisualStyleBackColor = true;
             this.cmdBuildHtmlRenderOne.Click += new System.EventHandler(this.cmdBuildHtmlRenderOne_Click);
             // 
-            // cmdBuild_HtmlRendererOnePortable
-            // 
-            this.cmdBuild_HtmlRendererOnePortable.Location = new System.Drawing.Point(291, 22);
-            this.cmdBuild_HtmlRendererOnePortable.Name = "cmdBuild_HtmlRendererOnePortable";
-            this.cmdBuild_HtmlRendererOnePortable.Size = new System.Drawing.Size(226, 57);
-            this.cmdBuild_HtmlRendererOnePortable.TabIndex = 1;
-            this.cmdBuild_HtmlRendererOnePortable.Text = "BuildMerge HtmlRenderer.One Portable";
-            this.cmdBuild_HtmlRendererOnePortable.UseVisualStyleBackColor = true;
-            // 
             // cmdCopyNativeResources
             // 
-            this.cmdCopyNativeResources.Location = new System.Drawing.Point(25, 109);
-            this.cmdCopyNativeResources.Name = "cmdCopyNativeResources";
-            this.cmdCopyNativeResources.Size = new System.Drawing.Size(203, 63);
-            this.cmdCopyNativeResources.TabIndex = 2;
-            this.cmdCopyNativeResources.Text = "CopyNativeResources";
-            this.cmdCopyNativeResources.UseVisualStyleBackColor = true;
-            this.cmdCopyNativeResources.Click += new System.EventHandler(this.cmdCopyNativeResources_Click);
+            //this.cmdCopyNativeResources.Location = new System.Drawing.Point(25, 109);
+            //this.cmdCopyNativeResources.Name = "cmdCopyNativeResources";
+            //this.cmdCopyNativeResources.Size = new System.Drawing.Size(203, 63);
+            //this.cmdCopyNativeResources.TabIndex = 2;
+            //this.cmdCopyNativeResources.Text = "CopyNativeResources";
+            //this.cmdCopyNativeResources.UseVisualStyleBackColor = true;
+            //this.cmdCopyNativeResources.Click += new System.EventHandler(this.cmdCopyNativeResources_Click);
+            // 
+            // cmdBuildSelectedMergePro
+            // 
+            this.cmdBuildSelectedMergePro.Location = new System.Drawing.Point(228, 322);
+            this.cmdBuildSelectedMergePro.Name = "cmdBuildSelectedMergePro";
+            this.cmdBuildSelectedMergePro.Size = new System.Drawing.Size(102, 37);
+            this.cmdBuildSelectedMergePro.TabIndex = 22;
+            this.cmdBuildSelectedMergePro.Text = "Build Merge";
+            this.cmdBuildSelectedMergePro.UseVisualStyleBackColor = true;
+            this.cmdBuildSelectedMergePro.Click += new System.EventHandler(this.cmdBuildSelectedMergePro_Click);
+            // 
+            // lstAsmReferenceList
+            // 
+            this.lstAsmReferenceList.FormattingEnabled = true;
+            this.lstAsmReferenceList.Location = new System.Drawing.Point(336, 322);
+            this.lstAsmReferenceList.Name = "lstAsmReferenceList";
+            this.lstAsmReferenceList.Size = new System.Drawing.Size(260, 251);
+            this.lstAsmReferenceList.TabIndex = 21;
+            // 
+            // listView2
+            // 
+            this.listView2.FullRowSelect = true;
+            this.listView2.Location = new System.Drawing.Point(336, 97);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(260, 205);
+            this.listView2.TabIndex = 20;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // lstPreset
+            // 
+            this.lstPreset.FormattingEnabled = true;
+            this.lstPreset.Location = new System.Drawing.Point(163, 491);
+            this.lstPreset.Name = "lstPreset";
+            this.lstPreset.Size = new System.Drawing.Size(167, 82);
+            this.lstPreset.TabIndex = 19;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(602, 97);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(367, 476);
+            this.listView1.TabIndex = 18;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // cmdReadSln
+            // 
+            this.cmdReadSln.Location = new System.Drawing.Point(336, 32);
+            this.cmdReadSln.Name = "cmdReadSln";
+            this.cmdReadSln.Size = new System.Drawing.Size(102, 37);
+            this.cmdReadSln.TabIndex = 17;
+            this.cmdReadSln.Text = "Read Sln";
+            this.cmdReadSln.UseVisualStyleBackColor = true;
+            this.cmdReadSln.Click += new System.EventHandler(this.cmdReadSln_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(602, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "All projects";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Merge Plans";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 434);
-            this.Controls.Add(this.cmdCopyNativeResources);
-            this.Controls.Add(this.cmdBuild_HtmlRendererOnePortable);
+            this.ClientSize = new System.Drawing.Size(985, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdBuildSelectedMergePro);
+            this.Controls.Add(this.lstAsmReferenceList);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.lstPreset);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmdReadSln);
+            //this.Controls.Add(this.cmdCopyNativeResources);
             this.Controls.Add(this.cmdBuildHtmlRenderOne);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button cmdBuildHtmlRenderOne;
-        private System.Windows.Forms.Button cmdBuild_HtmlRendererOnePortable;
-        private System.Windows.Forms.Button cmdCopyNativeResources;
+        //private System.Windows.Forms.Button cmdCopyNativeResources;
+        private System.Windows.Forms.Button cmdBuildSelectedMergePro;
+        private System.Windows.Forms.ListBox lstAsmReferenceList;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListBox lstPreset;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button cmdReadSln;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Source/BuildMergeProject/Form1.cs
+++ Source/BuildMergeProject/Form1.cs
@@ -15,51 +15,68 @@ namespace BuildMergeProject
 
         private void cmdBuildHtmlRenderOne_Click(object sender, EventArgs e)
         {
-            MergeProject mergePro = CreateMergePixelFarmProject();
-            mergePro.MergeAndSave(DEV_DIR + "HtmlRenderer.One.csproj",
-               "HtmlRenderer.One",
-               "v2.0",
-               //"ICU_VER_54;NET20",
-               ";NET20, __DESKTOP__"+ ",NET20,PIXEL_FARM,PIXEL_FARM_NET20,__SKIA__, GL_ENABLE",//additional define constant,
-               new string[] {
-                  "System",
-                  "System.Drawing",
-                  "System.Windows.Forms",
-                  "System.XML",
-               });
+            //MergeProject mergePro = CreateMergePixelFarmProject();
+            //mergePro.MergeAndSave(DEV_DIR + "HtmlRenderer.One.csproj",
+            //   "HtmlRenderer.One",
+            //   "v2.0",
+            //   //"ICU_VER_54;NET20",
+            //   ";NET20, __DESKTOP__"+ ",NET20,PIXEL_FARM,PIXEL_FARM_NET20,__SKIA__, GL_ENABLE",//additional define constant,
+            //   new string[] {
+            //      "System",
+            //      "System.Drawing",
+            //      "System.Windows.Forms",
+            //      "System.XML",
+            //   });
         }
-        static MergeProject CreateMergePixelFarmProject()
-        {
-            MergeProject mergePro = new MergeProject();
-            mergePro.LoadSubProject(DEV_DIR + @"Deps\PixelFarm.One.HtmlRenderer.csproj");//this is auto-gen project too             
-            //mergePro.LoadSubProject(DEV_DIR + @"Deps\PixelFarm.SkiaSharp\PixelFarm.SkiaSharp.csproj");
-            //mergePro.LoadSubProject(DEV_DIR + @"Deps\PixelFarm.Drawing.Skia\PixelFarm.Drawing.Skia.csproj");
-            //mergePro.LoadSubProject(DEV_DIR + @"Deps\Tesselate\Tesselate.csproj");
-            //------------------------------------------------------------------------------------------------
-            //base modules
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.ClientPlatforms\LayoutFarm.ClientPlatforms.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.CssBase\LayoutFarm.CssBase.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.WebLexer\LayoutFarm.WebLexer.csproj");
-            //layout and renderer module
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.BaseRenderer\LayoutFarm.BaseRenderer.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.HtmlRenderer\LayoutFarm.HtmlRenderer.csproj");
-            //integrated module
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.HtmlComposer\LayoutFarm.HtmlComposer.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.TextEdit\LayoutFarm.TextEdit.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.WebDom\LayoutFarm.WebDom.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.YourCustomWidgets\LayoutFarm.YourCustomWidgets.csproj");
-            //platforms.Windows
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.PlatformBase\LayoutFarm.PlatformBase.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.Platforms.WinForms\LayoutFarm.Platforms.WinForms.csproj");
+        //static MergeProject CreateMergePixelFarmProject()
+        //{
+        //    MergeProject mergePro = new MergeProject();
+        //    mergePro.LoadSubProject(DEV_DIR + @"Deps\PixelFarm.One.HtmlRenderer.csproj");//this is auto-gen project too             
+        //    //mergePro.LoadSubProject(DEV_DIR + @"Deps\PixelFarm.SkiaSharp\PixelFarm.SkiaSharp.csproj");
+        //    //mergePro.LoadSubProject(DEV_DIR + @"Deps\PixelFarm.Drawing.Skia\PixelFarm.Drawing.Skia.csproj");
+        //    //mergePro.LoadSubProject(DEV_DIR + @"Deps\Tesselate\Tesselate.csproj");
+        //    //------------------------------------------------------------------------------------------------
+        //    //base modules
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.ClientPlatforms\LayoutFarm.ClientPlatforms.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.CssBase\LayoutFarm.CssBase.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.WebLexer\LayoutFarm.WebLexer.csproj");
+        //    //layout and renderer module
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.BaseRenderer\LayoutFarm.BaseRenderer.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.HtmlRenderer\LayoutFarm.HtmlRenderer.csproj");
+        //    //integrated module
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.HtmlComposer\LayoutFarm.HtmlComposer.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.TextEdit\LayoutFarm.TextEdit.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.WebDom\LayoutFarm.WebDom.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.YourCustomWidgets\LayoutFarm.YourCustomWidgets.csproj");
+        //    //platforms.Windows
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.PlatformBase\LayoutFarm.PlatformBase.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"LayoutFarm.Platforms.WinForms\LayoutFarm.Platforms.WinForms.csproj");
 
-            //js engine
-            mergePro.LoadSubProject(DEV_DIR + @"Deps\Espresso\Net20\Espresso.Interfaces\Espresso.Interfaces.csproj");
-            mergePro.LoadSubProject(DEV_DIR + @"Deps\Espresso\Net20\EspressoCore\EspressoCore.csproj");
-            return mergePro;
-        }
-        private void cmdCopyNativeResources_Click(object sender, EventArgs e)
+        //    //js engine
+        //    mergePro.LoadSubProject(DEV_DIR + @"Deps\Espresso\Net20\Espresso.Interfaces\Espresso.Interfaces.csproj");
+        //    mergePro.LoadSubProject(DEV_DIR + @"Deps\Espresso\Net20\EspressoCore\EspressoCore.csproj");
+        //    return mergePro;
+        //}
+       
+        SolutionMx slnMx;
+        SolutionListViewController _slnListViewController;
+        private void cmdReadSln_Click(object sender, EventArgs e)
         {
-            //
+            //read sln file 
+            slnMx = new SolutionMx();
+            slnMx.ReadSolution(@"D:\projects\HTML-Renderer\Source\MainDev.sln");
+            _slnListViewController = new SolutionListViewController();
+            _slnListViewController.SetSolutionListView(this.listView1);
+            _slnListViewController.SetMergePlanListView(this.listView2);
+            _slnListViewController.SetProjectReferenceListView(this.lstAsmReferenceList);
+            _slnListViewController.LoadSolutionMx(slnMx);
         }
+
+        private void cmdBuildSelectedMergePro_Click(object sender, EventArgs e)
+        {
+            _slnListViewController.BuildMergeProjectFromSelectedItem();
+        }
+
+       
     }
 }
