afbaf482791075de9e3edd21d570b67659de1151
win <wintercoredev@gmail.com>
2017-11-26 21:13:21 +0700

    9274: update missing files

--- Source/Test3_MixHtml/ExampleFolderConfig.cs
+++ Source/Test3_MixHtml/ExampleFolderConfig.cs
@@ -1,6 +1,6 @@
 ﻿//Apache2, 2014-2017, WinterDev
 
-
+using Typography.TextBreak;
 namespace TestGraphicPackage2
 {
     static class ExampleFolderConfig
@@ -9,17 +9,14 @@ namespace TestGraphicPackage2
         static ExampleFolderConfig()
         {
             string execLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            execFromFolder = System.IO.Path.GetDirectoryName(execLocation);
-
-            LayoutFarm.TextBreak.CustomBreakerBuilder.DataDir = execFromFolder + "\\dictionaries"; 
-            //@"Deps_I18N\LayoutFarm.TextBreak\icu58\brkitr_src\dictionaries";
-
+            execFromFolder = System.IO.Path.GetDirectoryName(execLocation); 
+            Typography.TextBreak.CustomBreakerBuilder.DataDir = execFromFolder + "\\dictionaries"; 
+            //@"Deps_I18N\LayoutFarm.TextBreak\icu58\brkitr_src\dictionaries"; 
         }
-        public static void InitIcuData()
-        {
-                
-            string icu_dataFile = @"icudt57l.dat";
-            LayoutFarm.TextBreak.ICU.NativeTextBreaker.SetICUDataFile(execFromFolder + "\\" + icu_dataFile);
+        public static void InitData()
+        { 
+            //string icu_dataFile = @"icudt57l.dat";
+            //Typography.TextBreak.ICU.NativeTextBreaker.SetICUDataFile(execFromFolder + "\\" + icu_dataFile);
         }
 
     }
