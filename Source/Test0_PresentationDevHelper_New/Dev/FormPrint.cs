6d74a457e6fd04246143b400c4063d24cafccd25
win <wintercoredev@gmail.com>
2017-11-29 16:31:11 +0700

    9285: add save render surface to image file

--- Source/Test0_PresentationDevHelper_New/Dev/FormPrint.cs
+++ Source/Test0_PresentationDevHelper_New/Dev/FormPrint.cs
@@ -20,31 +20,31 @@ namespace LayoutFarm.Dev
         }
         private void cmdPrint_Click(object sender, EventArgs e)
         {
-            using (var bmp = new Bitmap(800, 600, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
-            using (var g = System.Drawing.Graphics.FromImage(bmp))
-            {
+            //using (var bmp = new Bitmap(800, 600, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            //using (var g = System.Drawing.Graphics.FromImage(bmp))
+            //{
 
-                //var canvasInit = new PixelFarm.Drawing.CanvasInitParameters();
-                //canvasInit.externalCanvas = g;
-                var canvas = new PixelFarm.Drawing.WinGdi.MyGdiPlusCanvas(0, 0, 800, 600); ;// LayoutFarm.UI.GdiPlus.MyWinGdiPortal.P.CreateCanvas(0, 0, 800, 600, canvasInit);
-                vwport.PrintMe(canvas);
-                bmp.Save("d:\\WImageTest\\testhtml.bmp");
-            }
+            //    //var canvasInit = new PixelFarm.Drawing.CanvasInitParameters();
+            //    //canvasInit.externalCanvas = g;
+            //    var canvas = new PixelFarm.Drawing.WinGdi.MyGdiPlusCanvas(0, 0, 800, 600); ;// LayoutFarm.UI.GdiPlus.MyWinGdiPortal.P.CreateCanvas(0, 0, 800, 600, canvasInit);
+            //    vwport.PrintMe(canvas);
+            //    bmp.Save("d:\\WImageTest\\testhtml.bmp");
+            // }
 
         }
 
         private void cmdPrintToPrinter_Click(object sender, EventArgs e)
         {
-            System.Drawing.Printing.PrintDocument printdoc = new System.Drawing.Printing.PrintDocument();
-            printdoc.PrintPage += (e2, s2) =>
-            {
-                var g = s2.Graphics;
-                //var canvasInit = new PixelFarm.Drawing.CanvasInitParameters();
-                //canvasInit.externalCanvas = g;
-                var canvas = new PixelFarm.Drawing.WinGdi.MyGdiPlusCanvas(0, 0, 800, 600);
-                vwport.PrintMe(canvas);
-            };
-            printdoc.Print();
+            //System.Drawing.Printing.PrintDocument printdoc = new System.Drawing.Printing.PrintDocument();
+            //printdoc.PrintPage += (e2, s2) =>
+            //{
+            //    var g = s2.Graphics;
+            //    //var canvasInit = new PixelFarm.Drawing.CanvasInitParameters();
+            //    //canvasInit.externalCanvas = g;
+            //    var canvas = new PixelFarm.Drawing.WinGdi.MyGdiPlusCanvas(0, 0, 800, 600);
+            //    vwport.PrintMe(canvas);
+            //};
+            //printdoc.Print();
         }
     }
 }
