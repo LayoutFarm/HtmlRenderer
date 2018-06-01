ab63902c52da232a631442c2f70f2e69f7c4db4d
win <wintercoredev@gmail.com>
2018-06-02 00:31:50 +0700

    151: IEventListener

--- Source/LayoutFarm.WebDom/2_WebDom/DomElement_EventListener.cs
+++ Source/LayoutFarm.WebDom/2_WebDom/DomElement_EventListener.cs
@@ -6,63 +6,72 @@ namespace LayoutFarm.WebDom
 {
     partial class DomElement : IUIEventListener
     {
-        void IUIEventListener.ListenKeyPress(UIKeyEventArgs e)
+        void IEventListener.ListenKeyPress(UIKeyEventArgs e)
         {
             OnKeyPress(e);
         }
-        void IUIEventListener.ListenKeyDown(UIKeyEventArgs e)
+        void IEventListener.ListenKeyDown(UIKeyEventArgs e)
         {
             OnKeyDown(e);
         }
-        void IUIEventListener.ListenKeyUp(UIKeyEventArgs e)
+        void IEventListener.ListenKeyUp(UIKeyEventArgs e)
         {
             OnKeyUp(e);
         }
-        bool IUIEventListener.ListenProcessDialogKey(UIKeyEventArgs e)
+        bool IEventListener.ListenProcessDialogKey(UIKeyEventArgs e)
         {
             return OnProcessDialogKey(e);
         }
-        void IUIEventListener.ListenMouseDown(UIMouseEventArgs e)
+        void IEventListener.ListenMouseDown(UIMouseEventArgs e)
         {
             OnMouseDown(e);
         }
-        void IUIEventListener.ListenLostMouseFocus(UIMouseEventArgs e)
+        void IEventListener.ListenLostMouseFocus(UIMouseEventArgs e)
         {
             OnLostMouseFocus(e);
         }
-        void IUIEventListener.ListenMouseMove(UIMouseEventArgs e)
+        void IEventListener.ListenMouseMove(UIMouseEventArgs e)
         {
             OnMouseMove(e);
         }
-        void IUIEventListener.ListenMouseUp(UIMouseEventArgs e)
+        void IEventListener.ListenMouseUp(UIMouseEventArgs e)
         {
             //1. mouse up
             OnMouseUp(e);
         }
-        void IUIEventListener.ListenMouseClick(UIMouseEventArgs e)
+        void IEventListener.ListenMouseClick(UIMouseEventArgs e)
         {
         }
-        void IUIEventListener.ListenMouseDoubleClick(UIMouseEventArgs e)
+        void IEventListener.ListenMouseDoubleClick(UIMouseEventArgs e)
         {
             OnDoubleClick(e);
         }
-        void IUIEventListener.ListenMouseWheel(UIMouseEventArgs e)
+        void IEventListener.ListenMouseWheel(UIMouseEventArgs e)
         {
             OnMouseWheel(e);
         }
-        void IUIEventListener.ListenMouseLeave(UIMouseEventArgs e)
+        void IEventListener.ListenMouseLeave(UIMouseEventArgs e)
         {
             OnMouseLeave(e);
         }
-        void IUIEventListener.ListenGotKeyboardFocus(UIFocusEventArgs e)
+        void IEventListener.ListenGotKeyboardFocus(UIFocusEventArgs e)
         {
             OnGotFocus(e);
         }
-        void IUIEventListener.ListenLostKeyboardFocus(UIFocusEventArgs e)
+        void IEventListener.ListenLostKeyboardFocus(UIFocusEventArgs e)
         {
             OnLostFocus(e);
         }
+        bool IUIEventListener.AutoStopMouseEventPropagation
+        {
+            get { return false; }
+        }
+        void IEventListener.ListenInterComponentMsg(object sender, int msgcode, string msg)
+        {
+            this.OnInterComponentMsg(sender, msgcode, msg);
+        }
 
+        //--------------
         void IUIEventListener.HandleContentLayout()
         {
             OnContentLayout();
@@ -79,16 +88,9 @@ namespace LayoutFarm.WebDom
         {
             get { return false; }
         }
-        bool IUIEventListener.AutoStopMouseEventPropagation
-        {
-            get { return false; }
-        }
-        void IUIEventListener.ListenInterComponentMsg(object sender, int msgcode, string msg)
-        {
-            this.OnInterComponentMsg(sender, msgcode, msg);
-        }
+       
 
-        void IUIEventListener.ListenGuestTalk(UIGuestTalkEventArgs e)
+        void IEventListener.ListenGuestTalk(UIGuestTalkEventArgs e)
         {
         }
         void IUIEventListener.GetGlobalLocation(out int x, out int y)
