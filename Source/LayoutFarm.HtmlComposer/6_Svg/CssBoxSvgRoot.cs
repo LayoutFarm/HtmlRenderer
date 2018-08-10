a69b5015aec46e6c8a5485743a77bcded0dab916
win <wintercoredev@gmail.com>
2018-08-10 21:21:26 +0700

    217: review exact bounds again

--- Source/LayoutFarm.HtmlComposer/6_Svg/CssBoxSvgRoot.cs
+++ Source/LayoutFarm.HtmlComposer/6_Svg/CssBoxSvgRoot.cs
@@ -63,11 +63,9 @@ namespace LayoutFarm.HtmlBoxes
 
                 PixelFarm.CpuBlit.RectD bound = _renderVx.GetBounds();
 
-                //create 
-                PixelFarm.CpuBlit.ActualBitmap backimg = new PixelFarm.CpuBlit.ActualBitmap((int)bound.Width + 200, (int)bound.Height + 200);
+                //create
+                PixelFarm.CpuBlit.ActualBitmap backimg = new PixelFarm.CpuBlit.ActualBitmap((int)bound.Width + 10, (int)bound.Height + 10);
                 PixelFarm.CpuBlit.AggPainter painter = PixelFarm.CpuBlit.AggPainter.Create(backimg);
-
-
                 //TODO: review here
                 //temp fix
                 if (s_openfontTextService == null)
