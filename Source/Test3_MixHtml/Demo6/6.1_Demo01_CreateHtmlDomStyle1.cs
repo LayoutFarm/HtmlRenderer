86e7f32d8c72c24d1ae9d038e367b999a76c55c4
win <wintercoredev@gmail.com>
2018-08-23 11:06:05 +0700

    278.1

---
+++ Source/Test3_MixHtml/Demo6/6.1_Demo01_CreateHtmlDomStyle1.cs
@@ -0,0 +1,60 @@
+﻿//MIT, 2014-present, WinterDev
+using LayoutFarm.WebDom;
+using LayoutFarm.UI;
+using LayoutFarm.WebWidgets;
+
+namespace LayoutFarm.Demo
+{
+    [DemoNote("6.1 Demo01_CreateHtmlDomStyle1")]
+    class Demo01_CreateHtmlDomStyle1 : HtmlDemoBase
+    {
+
+        public Demo01_CreateHtmlDomStyle1()
+        {
+        }
+        protected override void OnStart(AppHost host)
+        {
+            base.OnStart(host);//setup
+            
+            var htmldoc = this._groundHtmlDoc;
+            var rootNode = htmldoc.RootNode;
+            //1. create body node             
+            // and content 
+
+            DomElement body, div, span;
+            //style 1
+            rootNode.AddChild("body")
+                        .AddChild("div", out div)
+                            .AddChild("span", out span);
+            //-------------------------------------------- 
+            span.AddTextContent("ABCD");
+            //2. add to view 
+
+            //3. attach event to specific span
+            span.AttachEvent(UIEventName.MouseDown, e =>
+            {
+                //-------------------------------
+                //mousedown on specific span !
+                //-------------------------------
+#if DEBUG
+                // System.Diagnostics.Debugger.Break();
+                //Console.WriteLine("span");
+#endif
+                //test stop propagation 
+                e.StopPropagation();
+            });
+            div.AttachEvent(UIEventName.MouseDown, e =>
+            {
+#if DEBUG
+                //this will not print 
+                //if e has been stop by its child
+                // System.Diagnostics.Debugger.Break();
+                //Console.WriteLine("div");
+#endif
+
+            });
+
+        }
+
+    }
+}
---
+++ Source/Test3_MixHtml/Demo6/6.5_Demo05_Dynamic_BoxSpec.cs
@@ -0,0 +1,134 @@
+﻿//MIT, 2014-present, WinterDev
+using LayoutFarm.Composers;
+using LayoutFarm.UI;
+using LayoutFarm.WebWidgets;
+namespace LayoutFarm.Demo
+{
+    [DemoNote("6.5 Demo05_Dynamic_BoxSpec")]
+    class Demo05_Dynamic_BoxSpec : HtmlDemoBase
+    {
+        public Demo05_Dynamic_BoxSpec()
+        {
+        }
+        protected override void OnStart(AppHost host)
+        {
+            base.OnStart(host); //setup
+
+            WebDom.DomElement rootNode = _groundHtmlDoc.RootNode;
+
+            //style 2, lambda and adhoc attach event
+            rootNode.AddChild("body", body =>
+            {
+                body.AddChild("div", div =>
+                {
+                    div.AddChild("span", span =>
+                    {
+                        span.AddTextContent("ABCD");
+                        //3. attach event to specific span
+                        span.AttachMouseDownEvent(e =>
+                        {
+#if DEBUG
+
+                            var s_span = new EaseScriptElement(span);
+                            s_span.ChangeFontColor(PixelFarm.Drawing.Color.Blue);
+#endif
+
+                            e.StopPropagation();
+                        });
+                    });
+                    div.AddChild("span", span =>
+                    {
+                        span.AddTextContent("EFGHIJK");
+                        span.AttachMouseDownEvent(e =>
+                        {
+                            span.ClearAllElements();
+                            span.AddTextContent("LMNOP0003");
+                            var s_span = new EaseScriptElement(span);
+                            s_span.ChangeFontColor(PixelFarm.Drawing.Color.Red);
+                            s_span.ChangeBackgroundColor(PixelFarm.Drawing.Color.Yellow);
+                        });
+                        span.AttachMouseUpEvent(e =>
+                        {
+                            var s_span = new EaseScriptElement(span);
+                            s_span.ChangeFontColor(PixelFarm.Drawing.Color.Black);
+                            s_span.ChangeBackgroundColor(PixelFarm.Drawing.Color.White);
+                        });
+                    });
+                    //----------------------
+                    div.AttachEvent(UIEventName.MouseDown, e =>
+                    {
+#if DEBUG
+                        //this will not print 
+                        //if e has been stop by its child
+                        // System.Diagnostics.Debugger.Break();
+                        //Console.WriteLine("div");
+#endif
+
+                    });
+                });
+            });
+        }
+        //        protected override void OnStartDemo(HtmlPanel panel)
+        //        {
+        //            var htmldoc = panel.HtmlHost.CreatePresentationHtmlDoc();
+        //            var rootNode = htmldoc.RootNode;
+        //            //1. create body node             
+        //            // and content  
+
+        //            //style 2, lambda and adhoc attach event
+        //            rootNode.AddChild("body", body =>
+        //            {
+        //                body.AddChild("div", div =>
+        //                {
+        //                    div.AddChild("span", span =>
+        //                    {
+        //                        span.AddTextContent("ABCD");
+        //                        //3. attach event to specific span
+        //                        span.AttachMouseDownEvent(e =>
+        //                        {
+        //#if DEBUG
+
+        //                            var s_span = new EaseScriptElement(span);
+        //                            s_span.ChangeFontColor(PixelFarm.Drawing.Color.Blue);
+        //#endif
+
+        //                            e.StopPropagation();
+        //                        });
+        //                    });
+        //                    div.AddChild("span", span =>
+        //                    {
+        //                        span.AddTextContent("EFGHIJK");
+        //                        span.AttachMouseDownEvent(e =>
+        //                        {
+        //                            span.ClearAllElements();
+        //                            span.AddTextContent("LMNOP0003");
+        //                            var s_span = new EaseScriptElement(span);
+        //                            s_span.ChangeFontColor(PixelFarm.Drawing.Color.Red);
+        //                            s_span.ChangeBackgroundColor(PixelFarm.Drawing.Color.Yellow);
+        //                        });
+        //                        span.AttachMouseUpEvent(e =>
+        //                        {
+        //                            var s_span = new EaseScriptElement(span);
+        //                            s_span.ChangeFontColor(PixelFarm.Drawing.Color.Black);
+        //                            s_span.ChangeBackgroundColor(PixelFarm.Drawing.Color.White);
+        //                        });
+        //                    });
+        //                    //----------------------
+        //                    div.AttachEvent(UIEventName.MouseDown, e =>
+        //                    {
+        //#if DEBUG
+        //                        //this will not print 
+        //                        //if e has been stop by its child
+        //                        // System.Diagnostics.Debugger.Break();
+        //                        //Console.WriteLine("div");
+        //#endif
+
+        //                    });
+        //                });
+        //            });
+        //            //2. add to view 
+        //            panel.LoadHtmlDom(htmldoc,
+        //               LayoutFarm.Composers.CssDefaults.DefaultStyleSheet);
+        //        }
+    }
+}
