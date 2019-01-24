084ccfa4a8fb6c71c5fe85a3c5e8174d716b5afe
win <wintercoredev@gmail.com>
2019-01-25 04:00:05 +0700

    367: add GdiBitmapIO

--- Source/Test3_MixHtml/Program.cs
+++ Source/Test3_MixHtml/Program.cs
@@ -34,7 +34,7 @@ namespace TestGraphicPackage2
             Typography.TextBreak.CustomBreakerBuilder.Setup(dicProvider);
 #endif
 
-
+            PixelFarm.CpuBlit.MemBitmapExtensions.DefaultMemBitmapIO = new PixelFarm.Drawing.WinGdi.GdiBitmapIO();
 
 #if DEBUG
             PixelFarm.CpuBlit.Imaging.PngImageWriter.InstallImageSaveToFileService((IntPtr imgBuffer, int stride, int width, int height, string filename) =>
