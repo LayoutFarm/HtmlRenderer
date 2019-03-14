35a4c17abe51dfbc4d961bdd3bcc776ed9872931
win <wintercoredev@gmail.com>
2019-03-14 18:06:07 +0700

    410.1

--- Source/LayoutFarm.YourHtmlWidget/5.1_HtmlWidgetBase/ChoiceBox.cs
+++ Source/LayoutFarm.YourHtmlWidget/5.1_HtmlWidgetBase/ChoiceBox.cs
@@ -38,11 +38,11 @@ namespace LayoutFarm.HtmlWidgets
                 {
                     if (value)
                     {
-                        _imgNode.SetAttribute("src", OnlyOne ? "opt_checked.png" : "chk_checked.png");
+                        _imgNode.SetAttribute("src", OnlyOne ? WidgetResList.opt_checked_png : WidgetResList.chk_checked_png);
                     }
                     else
                     {
-                        _imgNode.SetAttribute("src", OnlyOne ? "opt_unchecked.png" : "chk_unchecked.png");
+                        _imgNode.SetAttribute("src", OnlyOne ? WidgetResList.opt_unchecked_png : WidgetResList.chk_unchecked_png);
                     }
                 }
             }
@@ -71,7 +71,7 @@ namespace LayoutFarm.HtmlWidgets
                 div2.SetAttribute("style", "color:black;");
                 _imgNode = div2.AddChild("img");
 
-                _imgNode.SetAttribute("src", OnlyOne ? "opt_unchecked.png" : "chk_unchecked.png");
+                _imgNode.SetAttribute("src", OnlyOne ? WidgetResList.opt_unchecked_png : WidgetResList.chk_unchecked_png);
 
                 _imgNode.AttachMouseDownEvent(e =>
                 {
--- Source/LayoutFarm.YourHtmlWidget/5.1_HtmlWidgetBase/WidgetResList.cs
+++ Source/LayoutFarm.YourHtmlWidget/5.1_HtmlWidgetBase/WidgetResList.cs
@@ -8,7 +8,7 @@ namespace LayoutFarm.HtmlWidgets
         //store 'built-in' resource list for our HtmlWidgetBase
         public const string opt_checked_png = "opt_checked.png";
         public const string opt_unchecked_png = "opt_unchecked.png";
-        public const string chk_checked_png = "opt_checked.png";
+        public const string chk_checked_png = "chk_checked.png";
         public const string chk_unchecked_png = "chk_unchecked.png";
     }
 }
